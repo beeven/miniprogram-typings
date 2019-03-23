d1f0f138905a953d2be53c74086d4f4b0089d2d0
NewFuture <New.Future@microsoft.com>
2019-03-23 15:27:34 +0800

    pack

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
- git clone 
- cp -rf test DefinitelyTyped/types/miniprogram/
- cp -rf types/wx DefinitelyTyped/types/miniprogram/
-
+git clone https://github.com/MS-DG/DefinitelyTyped.git
+cp -rf test DefinitelyTyped/types/miniprogram/
+cp -rf types/wx DefinitelyTyped/types/miniprogram/
+npx tslint --fix DefinitelyTyped/types/miniprogram/**/*.ts -p DefinitelyTyped/types/miniprogram/tsconfig.json
