6be0db5953fed9dfdf156bc2daf8a7640b81dd6a
NewFuture <New.Future@microsoft.com>
2019-07-17 09:35:10 +0800

    fix(rename): behavior

--- types/wx/index.d.ts
+++ types/wx/index.d.ts
@@ -17,6 +17,8 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
 /// <reference path="./lib.wx.api.d.ts" />
 /// <reference path="./lib.event.d.ts" />
 /// <reference path="./lib.wx.component.d.ts" />
+/// <reference path="./lib.wx.behavior.d.ts" />
+
 
 type IAnyObject = Record<string, any>;
 
