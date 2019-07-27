b9d94005b2968b331daeb48ea9ceeec609b8605f
Beeven Yip <beeven@hotmail.com>
2019-07-27 22:21:41 +0800

    Fixed ChooseLocation longtitude latitude type

--- types/wx/lib.wx.api.d.ts
+++ types/wx/lib.wx.api.d.ts
@@ -702,9 +702,9 @@ backgroundAudioManager.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb
     /** 详细地址 */
     address: string;
     /** 纬度，浮点数，范围为-90~90，负数表示南纬。使用 gcj02 国测局坐标系 */
-    latitude: string;
+    latitude: number;
     /** 经度，浮点数，范围为-180~180，负数表示西经。使用 gcj02 国测局坐标系 */
-    longitude: string;
+    longitude: number;
     /** 位置名称 */
     name: string;
   }
